01c2f55cef728c1f9a2f36f7e9094cebfeb0beec
kihangim <kihangim@gmail.com>
2020-01-17 14:58:58 +0900

    WIP

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -7,18 +7,37 @@ import video from "../../../Videos/CHI.mov";
 class Signin extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: ""
+    };
   }
+
+  inputemail = e => {
+    console.log(e.target.value);
+    this.setState({
+      email: e.target.value
+    });
+  };
+
+  handlePassword = e => {
+    console.log(e.target.value);
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  handleSignin = e => {
+    this.state.email.length > 5
+      ? alert("Welcome Color Hunt")
+      : alert("5글자 이상 입력해주세요");
+  };
+
   render() {
     return (
       <div>
         <Header />
-        {/* <div className="signin_container"></div>
-              <div className="loginLogo">
-                <img
-                  className="logo"
-                  alt="colorHunt_logo"
-                  src="https://colorhunt.co/img/color-hunt-palettes-logo.png" */}
+
         <div className="Signin">
           <div className="container">
             <div className="logoBox">
@@ -29,11 +48,9 @@ class Signin extends React.Component {
                 <div className="textLine">
                   Color Hunt 하나로 충분합니다.
                   <br />
-                  {/* <h4 className="textLine"> */}
                   원하는 색들을 조합해서 저장하고,
                   <br />
                   SNS에 공유해주세요!
-                  {/* </h4> */}
                 </div>
               </div>
               <div className="leftLogo">
@@ -45,11 +62,6 @@ class Signin extends React.Component {
                   loop
                   muted
                 ></video>
-                {/* <img
-                className="logo"
-                alt="colorHunt_logo"
-                src="https://colorhunt.co/img/color-hunt-palettes-logo.png"
-              /> */}
               </div>
             </div>
             <div className="signin_container">
@@ -65,14 +77,16 @@ class Signin extends React.Component {
               <div className="signinMain">
                 <div className="Field">
                   <input
+                    onChange={this.inputemail}
                     className="noBorder"
-                    type="name"
+                    type="text"
                     placeholder="카카오계정(이메일 또는 전호번호)"
+                    size="1"
                   />
                 </div>
-
                 <div className="Field">
                   <input
+                    onChange={this.handlePassword}
                     className="noBorder"
                     type="password"
                     placeholder="비밀번호"
@@ -82,7 +96,13 @@ class Signin extends React.Component {
                   <div className="radioButton"></div>
                   <span className="keepLogin">로그인 상태 유지</span>
                 </div>
-                <button className="loginButton">로그인</button>
+                <button
+                  onClick={this.handleSignin}
+                  className="loginButton"
+                  type="submit"
+                >
+                  로그인
+                </button>
               </div>
               <div className="signinBottom">
                 <div className="signup">회원가입</div>
